685b2f087504a4f4d470d36ebcd1b09664b8ea3c
Yegor Gunko <57867800+yegorgunko@users.noreply.github.com>
2021-06-05 13:14:23 +0300

    setup basic layout

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,17 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: [
+    'plugin:vue/essential',
+    'eslint:recommended',
+    '@vue/prettier',
+    'prettier',
+  ],
   parserOptions: {
-    parser: "babel-eslint",
+    parser: 'babel-eslint',
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
-};
+}
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,116 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="w-full flex justify-around">
+      <textarea v-model="source" class="w-full" @input="parse" />
+      <textarea v-model="rpy" class="w-full" readonly />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import MdIt from 'markdown-it'
+
+const md = new MdIt('commonmark')
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
+  name: 'App',
+  data() {
+    return {
+      source: '# label_name',
+      rpy: '',
+      indentLevel: 0,
+    }
   },
-};
-</script>
+  computed: {
+    ast() {
+      return md.parse(this.source)
+    },
+  },
+  created() {
+    this.parse()
+  },
+  methods: {
+    parse() {
+      const ast = this.ast
+      let line = 0
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+      this.rpy = ''
+
+      for (line; line < ast.length; line += 1) {
+        const { type } = ast[line]
+        let content = ast[line].content.trim()
+
+        switch (type) {
+          case 'heading_open': {
+            this.rpy += `label ${ast[++line].content}:`
+
+            this.rpy += '\n'
+
+            this.indentLevel += 1
+
+            break
+          }
+
+          case 'bullet_list_open': {
+            this.rpy += this.indent()
+
+            this.rpy += 'menu:\n'
+
+            this.indentLevel += 1
+
+            break
+          }
+
+          case 'list_item_open': {
+            this.rpy += this.indent()
+
+            this.rpy += `"${ast[(line += 2)].content}":\n`
+
+            this.indentLevel += 1
+
+            break
+          }
+
+          case 'inline': {
+            this.rpy += this.indent()
+
+            const [id, text] = content.split(' - ')
+            this.rpy += `${text ? `${id} "${text}"` : `"${id}"`}`
+
+            this.rpy += '\n'
+
+            break
+          }
+
+          case 'list_item_close': {
+            this.indentLevel -= 1
+
+            break
+          }
+
+          case 'bullet_list_close': {
+            this.indentLevel -= 1
+
+            break
+          }
+
+          case 'html_block': {
+            this.rpy += this.indent()
+
+            this.rpy += content.replace('<!--', '#').replace('-->', '').trim()
+
+            this.rpy += '\n'
+
+            break
+          }
+        }
+      }
+    },
+    indent() {
+      return Array(this.indentLevel * 4)
+        .fill(' ')
+        .join('')
+    },
+  },
 }
-</style>
+</script>
